290ed68bc1938c906add5d5cbd44732e392c94e1
peter-fogg <peter.fogg@oberlin.edu>
2013-04-12 17:54:34 -0400

    Added wee comments.

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 class Tree():
+    'Minimal binary tree class for testing.'
     
     def __init__(self, value):
         self.right = None
@@ -6,9 +7,11 @@ class Tree():
         self.value = value
     
     def is_leaf(self):
+        'A leaf has no children on right or left.'
         return self.right is None and self.left is None
     
     def preorder(self):
+        'Returns a string representing this tree as a preorder traversal.'
         s = self.value + '\n'
         if self.left:
             s += self.left.preorder()
@@ -16,18 +19,24 @@ class Tree():
             s += self.right.preorder()
         return s
     
-    def nodes_dict(self):
-        nodes = {self: self.value}
-        if self.left:
-            nodes.update(self.left.nodes_dict())
-        if self.right:
-            nodes.update(self.right.nodes_dict())
-        return nodes
+    # def nodes_dict(self):
+    #     nodes = {self: self.value}
+    #     if self.left:
+    #         nodes.update(self.left.nodes_dict())
+    #     if self.right:
+    #         nodes.update(self.right.nodes_dict())
+    #     return nodes
     
     def graphviz_id(self, i):
+        'Give a unique dot identifier for this node.'
         return 'node_%s' % str(i) if i >= 0 else '_'+str(-i)
     
     def graphviz_nodes(self, i=0):
+        '''
+        Returns all the nodes of the tree in dot format, i.e.,
+        
+        node_name [label=foo]
+        '''
         s = '%s [label=%s]\n' % (self.graphviz_id(i), self.value)
         if self.left:
             s += self.left.graphviz_nodes(i + 1)
@@ -36,6 +45,11 @@ class Tree():
         return s
     
     def graphviz_edges(self, i=0):
+        '''
+        Returns all the edges of the tree in dot format, i.e.,
+        
+        node_name -> other_node [label=foo]
+        '''
         s = ''
         if self.left:
             s += '%s -> %s\n' % (self.graphviz_id(i), self.left.graphviz_id(i + 1))
@@ -49,19 +63,28 @@ class Tree():
         return self.preorder()
 
 def read_tree(filename):
+    '''
+    Reads in a tree. Input should be a filename, the contents of which
+    are formatted as
+    
+    value [0|1] [0|1]
+    
+    where a 0 indicates that a tree should be popped and made either
+    the left or right child, respectively.
+    '''
     stack = []
     for line in open(filename):
         vals = line.split(' ')
         t = Tree(vals[0])
         if vals[1] == '1':
-            t.right = stack.pop()
-        if vals[2] == '1':
             t.left = stack.pop()
+        if vals[2] == '1':
+            t.right = stack.pop()
         stack.append(t)
     return stack.pop()
 
 def to_graphviz(tree):
-    nodes = tree.nodes_dict()
+    'Returns the dot represention of this tree. Nothing too fancy.'
     s = 'digraph {\n'
     s += tree.graphviz_nodes()
     s += tree.graphviz_edges()
@@ -69,4 +92,8 @@ def to_graphviz(tree):
     s += '\n}'
     return s
 
-print(to_graphviz(read_tree('treetest')))
+def main():
+    print(to_graphviz(read_tree('treetest')))
+
+if __name__ == '__main__':
+    main()
